0a2e07eec205eb68e8fd4b6a0a980867d7fd4420
jiangdl <jiangdl@hotmail.com>
2018-07-20 22:24:08 +0800

    Merge branch 'dev' of https://github.com/Deliang/MyFirstProject into dev
    # Conflicts:
    #	src/com/jz/demo/HelloWorld.java

--- src/com/jz/demo/HelloWorld.java
+++ src/com/jz/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jz.demo;
 
 public class HelloWorld {
     public static void main (String args[]){
-        System.out.print("test multi commit");
+        System.out.print("test ffmufflti commit");
     }
 }
