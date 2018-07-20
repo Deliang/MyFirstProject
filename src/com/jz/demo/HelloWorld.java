9190c64f0f1527e0d7ea4b3ad352af0fae9c118b
Deliang <jiangdl@hotmail.com>
2018-07-20 21:03:50 +0800

    Update HelloWorld.java

--- src/com/jz/demo/HelloWorld.java
+++ src/com/jz/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jz.demo;
 
 public class HelloWorld {
     public static void main (String args[]){
-        System.out.print("Hello woeryrld2");
+        System.out.print("Hello world on server");
     }
 }
