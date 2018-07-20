8e1faeb658724b03d2afd856925e75f1502c10a5
jiangdl <jiangdl@hotmail.com>
2018-07-20 20:50:55 +0800

    commit

--- src/com/jz/demo/HelloWorld.java
+++ src/com/jz/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jz.demo;
 
 public class HelloWorld {
     public static void main (String args[]){
-        System.out.print("Hello world2");
+        System.out.print("Hello woeryrld2");
     }
 }
